efc6de13c8ade6e53576c6639f1d8c321110953e
vcarvalho0 <vitor1.santos00@gmail.com>
2023-04-09 22:50:55 -0300

    feat: Config global style

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,16 @@
-import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
+import GlobalStyle from '@/src/styles/global'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>React Boilerplate</title>
+        <meta name="description" content="Simple next.js boilerplate" />
+      </Head>
+      <GlobalStyle />
+      <Component {...pageProps} />
+    </>
+  )
 }

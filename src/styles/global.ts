ea040f30615cd71213b2fd9b0aab050b6cfebbd0
vcarvalho0 <vitor1.santos00@gmail.com>
2023-04-22 00:01:46 -0300

    feat: Add new font

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,6 +1,39 @@
 import { createGlobalStyle, css } from 'styled-components'
 
 const GlobalStyle = createGlobalStyle`
+  @font-face {
+    font-display: swap;
+    font-family: 'Poppins';
+    font-style: normal;
+    font-weight: 300;
+    font-display: swap;
+    src: url('/fonts/poppins-v20-latin-300.eot'); /* IE9 Compat Modes */
+    src: url('/fonts/poppins-v20-latin-300.eot?#iefix') format('embedded-opentype'), /* IE6-IE8 */
+        url('/fonts/poppins-v20-latin-300.woff2') format('woff2'), /* Super Modern Browsers */
+  }
+
+  @font-face {
+    font-display: swap;
+    font-family: 'Poppins';
+    font-style: normal;
+    font-weight: 400;
+    font-display: swap;
+    src: url('/fonts/poppins-v20-latin-regular.eot'); /* IE9 Compat Modes */
+    src: url('/fonts/poppins-v20-latin-regular.eot?#iefix') format('embedded-opentype'), /* IE6-IE8 */
+        url('/fonts/poppins-v20-latin-regular.woff2') format('woff2'), /* Super Modern Browsers */
+  }
+
+  @font-face {
+    font-display: swap;
+    font-family: 'Poppins';
+    font-style: normal;
+    font-weight: 600;
+    font-display: swap;
+    src: url('/fonts/poppins-v20-latin-600.eot'); /* IE9 Compat Modes */
+    src: url('/fonts/poppins-v20-latin-600.eot?#iefix') format('embedded-opentype'), /* IE6-IE8 */
+        url('/fonts/poppins-v20-latin-600.woff2') format('woff2'), /* Super Modern Browsers */
+  }
+
   * {
     margin: 0;
     padding: 0;
